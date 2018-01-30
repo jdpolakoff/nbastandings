6d8eef7f8c1f2b9d7391b4c6f0d2e6500b42cb51
Jonathan Polakoff <jdpolakoff@gmail.com>
2018-01-29 21:42:38 -0500

    name

--- src/Homepage.js
+++ src/Homepage.js
@@ -29,14 +29,18 @@ class Homepage extends Component {
 
   render() {
 
+    var header = 'Buckets.life'
+
     return (
         <div>
           <Home teams={this.props.teams}/>
           <div className="typeWords">
+            <h1 className="words3">{header}</h1>
             <img className="bask" src={Basket} />
             <h2 className="words"></h2>
           </div>
           <div className="typeWords2">
+          <h1>{header}</h1>
           <img className="bask" src={Basket} />
             <p className="words2">Get NBA Playoff Standings. &#10003;<br/>
             Look Up Every Game of the NBA Season. &#10003;<br/>See who balled out. &#10003;<br/>Talk Smack. &#10003;</p>
